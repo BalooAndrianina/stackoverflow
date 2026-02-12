c0d8b33c916e58e84206b2d48652e753381744ef
BalooAndrianina <b.andrianina@gmail.com>
2026-02-12 16:32:27 +0100

    Création du boutoun flottant dans questionScreen, mais encore en commentaire car pas de fonction onclick

--- app/src/main/java/fr/mastersid/stackoverflow/ui/QuestionsScreen.kt
+++ app/src/main/java/fr/mastersid/stackoverflow/ui/QuestionsScreen.kt
@@ -9,6 +9,8 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.safeDrawing
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.Icon
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Switch
 import androidx.compose.material3.Text
@@ -18,6 +20,7 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -82,7 +85,16 @@ fun QuestionsScreen(modifier: Modifier){
                 notAnswered = checked
 
             }
-        }
+        },
+//        floatingActionButton = {
+//            FloatingActionButton(
+//                onClick =
+//            ) {
+//                Icon(painterResource(R.drawable.baseline_download_24),
+//                    stringResource(R.string.Update_questions)
+//                )
+//            }
+//        }
     ){ innerPadding ->
         LazyColumn(
             modifier = Modifier.padding(innerPadding),
