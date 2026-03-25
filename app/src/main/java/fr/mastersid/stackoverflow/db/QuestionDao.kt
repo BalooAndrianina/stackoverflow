f58cd8723bda22dd578ea28d4d12e0ad0aa09ea8
BalooAndrianina <b.andrianina@gmail.com>
2026-03-26 00:12:56 +0100

    TP4 : Création de l'interface DAO

---
+++ app/src/main/java/fr/mastersid/stackoverflow/db/QuestionDao.kt
@@ -0,0 +1,17 @@
+package fr.mastersid.stackoverflow.db
+
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import fr.mastersid.stackoverflow.data.Question
+import kotlinx.coroutines.flow.Flow
+
+interface QuestionDao {
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insertAll(list: List<Question>)
+
+    //creation de flow sur lequel je vais m'abonner via le VM
+    // @Query pour faire une requête SQL
+    @Query("SELECT * FROM question_table")
+    fun getQuestionListFlow(): Flow<List<Question>>
+}
