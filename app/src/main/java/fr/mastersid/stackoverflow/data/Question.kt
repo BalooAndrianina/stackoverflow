e510f546c7770703d9bf881cb1e5e2cddab527d9
BalooAndrianina <b.andrianina@gmail.com>
2026-03-26 00:05:03 +0100

    TP4 : désignation des lignes de la tabme de la BDD

--- app/src/main/java/fr/mastersid/stackoverflow/data/Question.kt
+++ app/src/main/java/fr/mastersid/stackoverflow/data/Question.kt
@@ -1,2 +1,12 @@
 package fr.mastersid.stackoverflow.data
-data class Question(val id : Int, val title : String,val body : String, val answerCount : Int)
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+// Transformation de la dataClass Question en une structure de table avec @Entity
+@Entity(tableName = "question_table")
+data class Question(@PrimaryKey val id: Int, val title: String, val body: String, val answerCount: Int)
+
+//Autre facon
+//@Entity(tableName = "question_table", primaryKeys = ["id"])
+//data class Question(val id: int, val title: String, val body: String, val answerCount: Int)
